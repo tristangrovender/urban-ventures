063af8b644eae331e1f04260266b9f12969f585c
Tristan Grovender <therealtristangrovender@gmail.com>
2020-04-16 16:17:57 -0500

    Project initialized with CRA, cleanup done, added dependencies, app is up and running with material-ui

---
+++ urban-ventures-project/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Home from "./views/Home.js";
+
+import "./App.css";
+
+function App() {
+    return (
+        <div className="app">
+            <Home />
+        </div>
+    );
+}
+
+export default App;
---
+++ urban-ventures-project/src/views/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Button from "@material-ui/core/Button";
+
+function Home() {
+    return (
+        <div className="home">
+            <Button variant="contained" color="primary">
+                Hello World
+            </Button>
+            <h2>navBar component goes here</h2>
+            <h1>Home view</h1>
+        </div>
+    );
+}
+
+export default Home;
